a73c13c7989ee0897a62c704bafa04ab207dc494
Chris <christian.jacome.mora@udla.edu.ec>
2024-10-10 14:10:54 -0500

    Update: Add route guard with authentication

--- src/router/index.js
+++ src/router/index.js
@@ -5,21 +5,42 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/auth',
+      name: 'login',
+      component: () => import('../views/LoginUser.vue'),
+    },
+    {
       path: '/',
       name: 'home',
-      component: ViewUsers
+      component: ViewUsers,
+      meta: { requiresAuth: true },
     },
     {
       path: '/add',
       name: 'add',
-      component: () => import('../views/AddUser.vue')
+      component: () => import('../views/AddUser.vue'),
     },
     {
-      path: '/edit/:id',
+      path: '/edit/:username',
       name: 'edit',
-      component: () => import('../views/UpdateUser.vue')
+      component: () => import('../views/UpdateUser.vue'),
+      meta: { requiresAuth: true },
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = localStorage.getItem('isAuthenticated') === 'true';
+
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!isAuthenticated) {
+      next('/auth');
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
 
 export default router
